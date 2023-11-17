1f80c177ff457da169821e4bf18488c251201d7b
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-11-17 14:57:24 +0100

    Add login design

--- app/src/main/java/com/example/odyssey/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/odyssey/activities/SplashScreenActivity.java
@@ -16,31 +16,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        /*
-         * Ovom opcijom je sakriven toolbar unutar ove aktivnosti
-         * */
-        int SPLASH_TIME_OUT = 3000;
+        int SPLASH_TIME_OUT = 5000;
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                /*
-                 * Intent je glavna klasa unutar Android-a za pokretanje ili prelazak na druge delove
-                 * vase aplikacije. Da bi pokrenuli drugu aktivnost imamo dve opcije
-                 * Prva opcija je eksplicitan intent, gde moramo da kazemo sa koje aktivnosti prelazimo na koju aktivnost:
-                 * NPR: sa SplashScreenActivity.this prelazimo na HomeActivity.class
-                 * Druga opcija je implicitni intent, gde ne moramo da kažemo gde prelazimo ali moramo
-                 * da kažemo šta planiramo da uradimo.
-                 */
-                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-                /*
-                 * Pozivom startActivity metode, saljemo poruku Android-u da on za nas pokrene drugu aktivnost,
-                 * nakon cega korisnik biva prebacen na novu aktivnost.
-                 **/
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(intent);
-                /*
-                 * Da ne bi moglo da se vrati na SplashScreen ako korisnik
-                 * klikne na back dugme
-                 * */
                 finish();
             }
         }, SPLASH_TIME_OUT);
