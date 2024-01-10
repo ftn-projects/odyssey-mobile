788295bbb047f6734c2bd894eba43cbf0129780a
Masa Ivanov <masa.ivano04.10@gmail.com>
2024-01-10 18:14:31 +0100

    Add reservation request card fragment

--- app/src/main/java/com/example/odyssey/clients/ReservationService.java
+++ app/src/main/java/com/example/odyssey/clients/ReservationService.java
@@ -1,4 +1,28 @@
 package com.example.odyssey.clients;
 
+import com.example.odyssey.model.accommodations.Reservation;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ReservationService {
+    @GET("host/{id}")
+    Call<List<Reservation>> getReservationsByHost(
+            @Path("id") Long id,
+            @Query("title") String title,
+            @Query("status") List<String> status,
+            @Query("startDate") Long startDate,
+            @Query("endDate") Long endDate
+    );
+
+    @PUT("status/{id}")
+    Call<ResponseBody> updateStatus(@Path("id") Long id, @Query("status") String status);
+}
---
+++ app/src/main/java/com/example/odyssey/fragments/ReservationRequest.java
@@ -0,0 +1,20 @@
+package com.example.odyssey.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.odyssey.R;
+
+public class ReservationRequest extends LinearLayout {
+
+    public ReservationRequest(Context context) {
+        super(context);
+    }
+}
