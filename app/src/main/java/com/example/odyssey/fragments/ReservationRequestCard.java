0119545c13c16744659473593d9e313fbe94d3d0
Masa Ivanov <masa.ivano04.10@gmail.com>
2024-01-11 11:16:01 +0100

    Add reservation request fragment and layout

---
+++ app/src/main/java/com/example/odyssey/fragments/ReservationRequestCard.java
@@ -0,0 +1,12 @@
+package com.example.odyssey.fragments;
+
+import android.content.Context;
+
+import android.widget.LinearLayout;
+
+public class ReservationRequestCard extends LinearLayout {
+
+    public ReservationRequestCard(Context context) {
+        super(context);
+    }
+}
